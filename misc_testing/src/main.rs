7a269a0b8faedd3257dc4f5d4370b6769ca3e781
Jonathan Jackson <theonethatis720@gmail.com>
2023-04-12 17:48:48 -0700

    Just testing some references and ownership concepts

--- misc_testing/src/main.rs
+++ misc_testing/src/main.rs
@@ -1,3 +1,11 @@
+
+
 fn main() {
-    println!("Hello, world!");
+	let mut s = String::from("hello");
+	
+	let r1 = &mut s;
+	
+    println!("{}", r1);
+    let r2 = &mut s;
+    println!("{}", r2);
 }
