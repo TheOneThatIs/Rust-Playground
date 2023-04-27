0d741c3fad6a85c25590699b4a26969e8f7e614c
Jonathan Jackson <theonethatis720@gmail.com>
2023-04-26 22:35:22 -0700

    Prototyped some cards

--- go_fish/src/main.rs
+++ go_fish/src/main.rs
@@ -1,10 +1,162 @@
 fn main() {
-    println!("╓─────────┐");
-    println!("║✾ ₪₪₪₪₪ ✾│");
-    println!("║ ▕▚▚▚▚▚▏ │");
-    println!("║ ▕▚▚▚▚▚▏ │");
-    println!("║ ▕▚▚▚▚▚▏ │");
-    println!("║ ▕▚▚▚▚▚▏ │");
-    println!("║✾ ₪₪₪₪₪ ✾│");
-    println!("╚═════════╛");
+    println!(" ______     ______        ______   __     ______     __  __   ");
+    println!(r"/\  ___\   /\  __ \      /\  ___\ /\ \   /\  ___\   /\ \_\ \");
+    println!(r"\ \ \__ \  \ \ \/\ \     \ \  __\ \ \ \  \ \___  \  \ \  __ \ ");
+    println!(r" \ \_____\  \ \_____\     \ \_\    \ \_\  \/\_____\  \ \_\ \_\ ");
+    println!(r"  \/_____/   \/_____/      \/_/     \/_/   \/_____/   \/_/\/_/");
+    
+    println!("╭─────────╮");
+    println!("│ 3       │");
+    println!("│         │");
+    println!("│      ♠  │");
+    println!("│    ♠    │");
+    println!("│  ♠      │");
+    println!("│         │");
+    println!("│       3 │");
+    println!("╰─────────╯");
+    
+    // DECK
+    println!("╭──────────╮");
+    println!("│ ╭────────┴╮");
+    println!("│ │✾ ₪₪₪₪₪ ✾│");
+    println!("│ │  ▚▚▚▚▚  │");
+    println!("│ │  ▚▚▚▚▚  │");
+    println!("│ │  ▚▚▚▚▚  │");
+    println!("│ │  ▚▚▚▚▚  │");
+    println!("│ │  ▚▚▚▚▚  │");
+    println!("╰─│✾ ₪₪₪₪₪ ✾│");
+    println!("  ╰─────────╯");
+    
+    // HAND
+    println!(" 1:  2:  3:");
+    println!("╭───╭───╭─────────╮");
+    println!("│ A │ 10│ 3       │");
+    println!("│ ♡ │ ♠ │         │");
+    println!("│   │   │      ♠  │");
+    println!("│   │   │    ♠    │");
+    println!("│   │   │  ♠      │");
+    println!("│   │   │         │");
+    println!("│   │   │       3 │");
+    println!("╰───╰───╰─────────╯");
+
+    
+    //println!("┌─────────┐");
+    //println!("│ 4       │");
+    //println!("│         │");
+    //println!("│  ♠   ♠  │");
+    //println!("│         │");
+    //println!("│  ♠   ♠  │");
+    //println!("│         │");
+    //println!("│       4 │");
+    //println!("└─────────┘");
+    //
+    //println!("╓─────────┐");
+    //println!("║ 5       │");
+    //println!("║         │");
+    //println!("║  ♠   ♠  │");
+    //println!("║    ♠    │");
+    //println!("║  ♠   ♠  │");
+    //println!("║         │");
+    //println!("║       5 │");
+    //println!("╚═════════╛");
+    //
+    //println!("╓─────────┐");
+    //println!("║ 6       │");
+    //println!("║         │");
+    //println!("║  ♠   ♠  │");
+    //println!("║  ♠   ♠  │");
+    //println!("║  ♠   ♠  │");
+    //println!("║         │");
+    //println!("║       6 │");
+    //println!("╚═════════╛");
+    //
+    //println!("╓─────────┐");
+    //println!("║ 7       │");
+    //println!("║         │");
+    //println!("║  ♠   ♠  │");
+    //println!("║ ♠  ♠  ♠ │");
+    //println!("║  ♠   ♠  │");
+    //println!("║         │");
+    //println!("║       7 │");
+    //println!("╚═════════╛");
+    //
+    //println!("╓─────────┐");
+    //println!("║ 8       │");
+    //println!("║    ♠    │");
+    //println!("║  ♠   ♠  │");
+    //println!("║ ♠     ♠ │");
+    //println!("║  ♠   ♠  │");
+    //println!("║    ♠    │");
+    //println!("║       8 │");
+    //println!("╚═════════╛");
+    //
+    //println!("╓─────────┐");
+    //println!("║ 9       │");
+    //println!("║    ♠    │");
+    //println!("║  ♠   ♠  │");
+    //println!("║ ♠  ♠  ♠ │");
+    //println!("║  ♠   ♠  │");
+    //println!("║    ♠    │");
+    //println!("║       9 │");
+    //println!("╚═════════╛");
+    
+    //println!("╭─────────╮");
+    //println!("│ ♧       │");
+    //println!("│  _  __  │");
+    //println!("│ │ │╱ ╱  │");
+    //println!("│ │ ' ╱   │");
+    //println!("│ │ . ╲   │");
+    //println!(r"│ │_│╲_╲  │");
+    //println!("│       ♧ │");
+    //println!("╰─────────╯");
+    
+    //println!("╭─────────╮");
+    //println!("│ ♧ ___   │");
+    //println!("│  ╱ _ ╲  │");
+   //println!(r"│ │ | | │ │");
+    //println!("│ │ |_| │ │");
+    //println!("│  ╲__╲_╲ │");
+   //println!(r"│         │");
+    //println!("│       ♧ │");
+    //println!("╰─────────╯");
+    
+    //println!("╭─────────╮");
+    //println!("│ ♧       │");
+    //println!("│  ┌─────┐│");
+    //println!("│  └─┐ ┌─┘│");
+    //println!("│┌─┐ │ │  │");
+    //println!("││ └─┘ │  │");
+    //println!("│ ╲___╱   │");
+    //println!("│       ♧ │");
+    //println!("╰─────────╯");
+    
+    println!("╭─────────╮");
+    println!("│      J♧ │");
+    println!("│      ██ │");
+    println!("│      ██ │");
+    println!("│      ██ │");
+    println!("│ ██   ██ │");
+    println!("│  █████  │");
+    println!("│ J♧      │");
+    println!("╰─────────╯");
+    
+    println!("╭─────────╮");
+    println!("│      Q♧ │");
+    println!("│  ██████ │");
+    println!("│ ██    ██│");
+    println!("│ ██    ██│");
+    println!("│ ██ ▄▄ ██│");
+    println!("│  ██████ │");
+    println!("│ Q♧  ▀▀  │");
+    println!("╰─────────╯");
+    
+    println!("╭─────────╮");
+    println!("│      K♧ │");
+    println!("│ ██   ██ │");
+    println!("│ ██  ██  │");
+    println!("│ █████   │");
+    println!("│ ██  ██  │");
+    println!("│ ██   ██ │");
+    println!("│ K♧      │");
+    println!("╰─────────╯");
 }
