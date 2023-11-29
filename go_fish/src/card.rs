8a8251b0663e59ad56c17b887b54e996016b9c0b
Jonathan Jackson <theonethatis720@gmail.com>
2023-11-28 18:36:17 -0800

    Started work on the card builder

--- go_fish/src/card.rs
+++ go_fish/src/card.rs
@@ -1,5 +1,16 @@
 use strum_macros::EnumIter;
 
+pub enum Type {
+    Middle
+    LeftEdge,
+    RightEdge,
+}
+
+pub enum Facing {
+    FaceUp,
+    FaceDown,
+}
+
 #[derive (Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, EnumIter)]
 pub enum Suit {
     Club,
@@ -35,4 +46,29 @@ impl Card {
     pub fn new(suit: Suit, rank: Rank) -> Card {
         Card { suit, rank }
     }
+}
+
+pub fn create_card(suit: Suit, rank: Rank, type: Type, facing: Facing) {
+    suit_map = [
+        (Suit::Club, '♣'),
+        (Suit::Diamond, '♦'),
+        (Suit::Spade, '♠'),
+        (Suit::Heart, '♥'),
+    ].iter().cloned.collect::<HashMap<Suit, char>>();
+
+    let rank_map = [
+        (Rank::Ace, "A"),
+        (Rank::Two, "2"),
+        (Rank::Three, "3"),
+        (Rank::Four, "4"),
+        (Rank::Five, "5"),
+        (Rank::Six, "6"),
+        (Rank::Seven, "7"),
+        (Rank::Eight, "8"),
+        (Rank::Nine, "9"),
+        (Rank::Ten, "10"),
+        (Rank::Jack, "J"),
+        (Rank::Queen, "Q"),
+        (Rank::King, "K"),
+    ].iter().cloned.collect::<HashMap<Rank, &str>>();
 }
